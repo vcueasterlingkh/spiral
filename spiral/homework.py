7c7f79dbe20fd36f3bd60f54e923fa289f247c90
vcueasterlingkh <78060887+vcueasterlingkh@users.noreply.github.com>
2021-04-09 22:51:01 -0400

    homework 9

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,25 @@
 def spiralize(number):
-    return_value = 1
+    if n < 1 or n % 2 ==0: return None
+    elif n == 1: return 1
+    
+    else:
+        numbers = [1]
+        numbers_needed = 2 * n -1
+        increment = 2
+        while len(numbers) < numbers_needed:
+            increment = += 2 
+            for p in range(4):
+                numbers.append(numbers[-1] + increment)     
+    return sum(numbers)
+if __name__ == "__main__":
+    start = time()
+    ans = spiralize(501)
+    elapsed_time = (timer() - start) * 1000 
+    if ans: 
+        print "Found Sum"
+    else:
+        print "No answer found"
+
 
     return return_value
+
